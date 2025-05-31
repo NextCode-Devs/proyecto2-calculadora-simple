1172b12cbb7ee5d847fc7b385070c61b2b70c27e
Cristian Franco <cristian.franko.cf@gmail.com>
2025-05-30 23:44:48 -0300

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ session_start();
 require_once __DIR__ . '/backend/includes/funciones.php';
 
 if (!isset($_SESSION['usuario_id'])) {
-    header('Location: ../../frontend/html/iniciosesion.html');
+    header('Location: frontend/html/iniciosesion.html');
     exit;
 }
 
